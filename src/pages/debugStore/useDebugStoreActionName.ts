5855f5452bd15435b3de63b373062976626d8c2c
권신학 <cheatkey.dev@gmail.com>
2023-09-02 17:04:04 +0900

    디버깅 예제 코드 정리

--- src/pages/debugStore/useDebugStoreActionName.ts
+++ src/pages/debugStore/useDebugStoreActionName.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import {
   Mutate,
   SetState,
@@ -16,95 +17,58 @@ type Entries<T> = {
 const getEntries = <T extends object>(obj: T) =>
   Object.entries(obj) as Entries<T>;
 
-const myWrapper =
+const actionAutoMatcher =
   <S>(setApi: NamedSet<S>) =>
-  // eslint-disable-next-line @typescript-eslint/ban-types
   <T extends Record<string, (set: SetState<S>) => Function>>(methods: T) => {
-    const a = getEntries(methods).map(([functionName, methodFn]) => {
+    const Store = getEntries(methods).map(([functionName, methodFn]) => {
       const originMethodFn = methodFn;
 
-      const newMethodFn = (...args) => {
+      const newMethodFn = (...args: unknown[]) => {
         const setter = (
           props: Parameters<NamedSet<S>>[0],
           replace: Parameters<NamedSet<S>>[1]
         ) => {
           setApi(props, replace, {
             type: functionName,
-            date: "heello",
             ...args,
           });
         };
 
-        console.log("래핑됨", args);
         return originMethodFn(setter)(...args);
       };
 
       return [functionName, newMethodFn];
     });
 
-    return Object.fromEntries(a) as {
+    return Object.fromEntries(Store) as {
       [P in keyof T]: ReturnType<T[P]>;
     };
   };
 
+interface IDebugStoreState {
+  count: number;
+}
+
+const initialState: IDebugStoreState = {
+  count: 0,
+};
+
 export const useDebugStoreActionName = create(
   devtools(
-    combine(
-      {
-        count: 0,
-      },
-      (_set) => {
-        return myWrapper(_set)({
-          increase: (set) => (name: string, age: number) => {
-            set((state) => ({
-              count: state.count + 1,
-            }));
-          },
-          decrease: (set) => () => {
-            set((state) => ({
-              count: state.count - 1,
-            }));
-            return true;
-          },
-        });
-
-        // const mapper = <
-        //   T extends Record<string, (set: typeof _set) => Function>
-        // >(
-        //   methods: T
-        // ) => {
-        //   const b = getEntries(methods);
-        //   const a = getEntries(methods).map(([key, method]) => {
-        //     const setter = (
-        //       props: Parameters<typeof _set>[0],
-        //       replace: Parameters<typeof _set>[1]
-        //     ) => {
-        //       _set(props, replace, {
-        //         type: key,
-        //       });
-        //     };
-
-        //     return [key, method(setter)];
-        //   }) as typeof b;
-
-        //   return Object.fromEntries(a) as {
-        //     [P in keyof T]: ReturnType<T[P]>;
-        //   };
-        // };
-
-        // return mapper({
-        //   increase: (set) => (by: number) => {
-        //     set((state) => ({
-        //       bears: state.bears + by,
-        //     }));
-        //   },
-        //   decrease: (set) => (by: number) => {
-        //     set((state) => ({
-        //       bears: state.bears - by,
-        //     }));
-        //   },
-        // });
-      }
+    combine(initialState, (_set) =>
+      actionAutoMatcher(_set)({
+        increase: (set) => (name: string, age: number) => {
+          set((state) => ({
+            count: state.count + 1,
+          }));
+        },
+        decrease: (set) => () => {
+          set((state) => ({
+            count: state.count - 1,
+          }));
+          return true;
+        },
+      })
     )
   )
 );
